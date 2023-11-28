1a6cade0f3bb445455619dff138c0db375a94334
Gyutae Bae <qoxowkd0716@gmail.com>
2023-11-28 20:30:37 +0900

    Spring Security : JWT 로그인

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -2,6 +2,7 @@ package com.sparta.springauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class SpringAuthApplication {
@@ -9,5 +10,6 @@ public class SpringAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringAuthApplication.class, args);
     }
+
 
 }
